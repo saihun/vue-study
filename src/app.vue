1999b145ec9bd89acaee20c29c21dddb02ca08f2
saihun <xjs_smile@126.com>
2018-09-27 09:59:11 +0800

    vue study

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<style scoped>
+    @import 'styles/common.css';
+</style>
+<template>
+    <div>
+        {{ message }}
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                message : "hello --------------"
+            }
+        },
+        mounted () {
+
+        },
+        beforeDestroy () {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
